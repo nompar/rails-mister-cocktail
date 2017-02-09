7dcda7993eb07d82e07115727d2ab8cd3068e22e
Hadrien de Caumont <hadrien.decaumont@gmail.com>
2017-02-09 12:54:21 +0100

    almost

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :coktails, only: [:index]
+  root to: 'cocktails#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ file = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+ json = JSON.parse(file.read.to_s)
+ json["drinks"].each do |hash|
+  Ingredient.create(name: hash["strIngredient1"])
+end
